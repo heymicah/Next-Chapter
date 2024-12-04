30ebb8485994dbd4319d455741863f93b809c33c
GiulianaSilva <Giulianasilva.e@gmail.com>
2024-12-03 19:12:41 -0500

    small changes

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import './Home.css'; // Import the CSS file
-import titleImage from './assets/Images/TitleImage.png'; // Import the image file
+import './Home.css'; 
+import titleImage from './assets/Images/TitleImage.png';
 
 function Home() {
   console.log('Home component rendered');
   return (
     <div className="home-container">
-      <img src={titleImage} alt="Title" className="home-image" /> {/* Use the imported image */}
+      <img src={titleImage} alt="Title" className="home-image" /> {}
       <div className="search-container">
-        <i className="fas fa-search search-icon"></i> {/* Font Awesome search icon */}
-        <input type="text" id="myTextInput" className="home-textbox" placeholder="Enter text here" /> {/* Corrected input element */}
+        <i className="fas fa-search search-icon"></i> {}
+        <input type="text" id="myTextInput" className="home-textbox" placeholder="Enter text here" /> {}
       </div>
     </div>
   );
